e1d33e51ad8c39185349424fde9ee36b12b297e6
Hatem Dhaouadi <94910192+heinzs222@users.noreply.github.com>
2026-07-16 06:39:45 +0100

    feat: route blogs index

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import BlogIndex from "./components/BlogIndex.jsx";
 import SeoPage, { SEO_PATHS } from "./components/SeoPages.jsx";
 import "./styles.css";
 import "./seo-pages.css";
+import "./blog-index.css";
 
 const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
-const content = SEO_PATHS.has(normalizedPath)
-  ? <SeoPage path={normalizedPath} />
-  : <App />;
+const content = normalizedPath === "/blogs"
+  ? <BlogIndex />
+  : SEO_PATHS.has(normalizedPath)
+    ? <SeoPage path={normalizedPath} />
+    : <App />;
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
