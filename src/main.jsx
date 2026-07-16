3c6ed5113538d725b3155dfe05b4ca876b2bbcea
Hatem Dhaouadi <94910192+heinzs222@users.noreply.github.com>
2026-07-16 06:56:15 +0100

    chore: apply simplified public copy

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import BlogIndex from "./components/BlogIndex.jsx";
 import SeoPage, { SEO_PATHS } from "./components/SeoPages.jsx";
+import { installPublicCopy } from "./lib/public-copy.js";
 import "./styles.css";
 import "./seo-pages.css";
 import "./blog-index.css";
 import "./mobile-navigation-fixes.css";
+
+installPublicCopy();
 
 const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
 const content = normalizedPath === "/blogs"
