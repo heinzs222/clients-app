e84c900cdf3333043d2944eabc396debb1908465
Hatem Dhaouadi <94910192+heinzs222@users.noreply.github.com>
2026-07-19 05:15:27 +0100

    Add the provider workspace route

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import BlogIndex from "./components/BlogIndex.jsx";
 import GhlBlogPage from "./components/GhlBlogPage.jsx";
+import PlatformsPage from "./components/PlatformsPage.jsx";
 import SeoPublicShell from "./components/SeoPublicShell.jsx";
 import ServicesPage from "./components/ServicesPage.jsx";
 import { SEO_PATHS } from "./components/SeoPages.jsx";
@@ -14,6 +15,7 @@ import "./home-beginner-seo.css";
 import "./client-boundaries-visual.css";
 import "./revenue-offers.css";
 import "./services-page.css";
+import "./platforms-page.css";
 import "./seo-pages.css";
 import "./seo-public-shell.css";
 import "./blog-index.css";
@@ -22,19 +24,22 @@ import "./mobile-navigation-fixes.css";
 import "./lib/public-copy.js";
 import "./lib/home-enhancements.js";
 import "./lib/revenue-offers.js";
+import "./lib/provider-navigation.js";
 
 initializeAnalytics();
 
 const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
-const content = normalizedPath === "/meta-capi-setup-service"
-  ? <ServicesPage />
-  : normalizedPath === "/blogs"
-    ? <BlogIndex />
-    : ALL_BLOG_PATHS.has(normalizedPath)
-      ? <GhlBlogPage path={normalizedPath} />
-      : SEO_PATHS.has(normalizedPath)
-        ? <SeoPublicShell path={normalizedPath} />
-        : <App />;
+const content = normalizedPath === "/platforms"
+  ? <PlatformsPage />
+  : normalizedPath === "/meta-capi-setup-service"
+    ? <ServicesPage />
+    : normalizedPath === "/blogs"
+      ? <BlogIndex />
+      : ALL_BLOG_PATHS.has(normalizedPath)
+        ? <GhlBlogPage path={normalizedPath} />
+        : SEO_PATHS.has(normalizedPath)
+          ? <SeoPublicShell path={normalizedPath} />
+          : <App />;
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
